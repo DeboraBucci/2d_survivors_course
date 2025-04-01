0109b44a7291a1790873c62baf9f8202d673afa6
Debora Bucci <dbucci@protonmail.com>
2025-04-01 16:14:47 -0300

    modify drag time of camera

--- scenes/game_camera/game_camera.gd
+++ scenes/game_camera/game_camera.gd
@@ -6,10 +6,9 @@ func _ready():
 	make_current()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	acquire_target()
-	global_position = global_position.lerp(target_position, 1.0 - exp(-delta ))
+	global_position = global_position.lerp(target_position, 1.0 - exp(-delta * 20))
 
 
 func acquire_target():
