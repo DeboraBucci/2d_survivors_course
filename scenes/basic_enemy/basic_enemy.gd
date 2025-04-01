ce17e41fd47f6e90eda43b486021232095821510
Debora Bucci <dbucci@protonmail.com>
2025-04-01 13:04:41 -0300

    create a sword and animate it

--- scenes/basic_enemy/basic_enemy.gd
+++ scenes/basic_enemy/basic_enemy.gd
@@ -2,12 +2,6 @@ extends CharacterBody2D
 
 const MAX_SPEED = 75
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var direction = get_direction_to_player()
 	velocity = direction * MAX_SPEED
