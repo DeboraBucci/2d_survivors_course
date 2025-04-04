e35898ed44a599e89e1e58174020e188ce9c1da8
Debora Bucci <dbucci@protonmail.com>
2025-04-04 10:17:50 -0300

    make player take damage, and color the health bar

--- scenes/component/health_component.gd
+++ scenes/component/health_component.gd
@@ -2,6 +2,7 @@ extends Node
 class_name HealthComponent
 
 signal died
+signal health_changed
 
 @export var max_health: float = 10
 var current_health
@@ -13,7 +14,16 @@ func damage(damage_amount: float):
 	if current_health > 0:
 		current_health -= damage_amount
 	
+	health_changed.emit()
 	Callable(check_death).call_deferred()
+
+
+func get_health_percent():
+	if max_health <= 0:
+		return 0
+	
+	return min(current_health / max_health, 1)
+
 
 func check_death():
 	if current_health == 0:
