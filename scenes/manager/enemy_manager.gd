fb49f3f084c5898ca94bae5bd141f1fb992510e8
Debora Bucci <dbucci@protonmail.com>
2025-04-04 12:15:33 -0300

    fix enemies spawning outside arena

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -15,6 +15,31 @@ func _ready():
 	timer.timeout.connect(on_timer_timeout)
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 
+
+func get_spawn_position():
+	timer.start()
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	
+	if player == null:
+		return Vector2.ZERO
+	
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	var spawn_position = Vector2.ZERO
+	for i in 4:
+		spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+		
+		var query_parameters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1 << 0)
+		var result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_parameters)
+	
+		if result.is_empty():
+			break;
+		
+		else:
+			random_direction = random_direction.rotated(deg_to_rad(90))
+	
+	return spawn_position
+
+
 func on_timer_timeout():
 	timer.start()
 	var player = get_tree().get_first_node_in_group("player") as Node2D
@@ -22,14 +47,11 @@ func on_timer_timeout():
 	if player == null:
 		return
 	
-	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
-	var spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
-	
 	var enemy = basic_enemey_scene.instantiate() as Node2D
 	
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	entities_layer.add_child(enemy)
-	enemy.global_position = spawn_position
+	enemy.global_position = get_spawn_position()
 
 
 func on_arena_difficulty_increased(arena_difficulty: int):
